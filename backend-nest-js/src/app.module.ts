b618147f6576214f8aabd4becb969687ee86b8c7
Matias F. Acevedo <matias.acevedo56732@gmail.com>
2024-03-12 21:40:01 -0300

    Connection with MySQL

--- backend-nest-js/src/app.module.ts
+++ backend-nest-js/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type:"mysql",
+      host:"localhost",
+      username:"root",
+      password:"root",
+      port:3306,
+      database:"db_clickshop",
+      entities:[__dirname + "/**/*.entity{.ts,.js}"],
+      synchronize:true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
